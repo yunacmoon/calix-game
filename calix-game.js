404e8df3fa1779a807c01513185810f2603e6aa0
Yuna Moon <yunamoon@Yunas-MacBook-Pro-2.local>
2026-04-06 17:18:34 +1000

    feat: apply EP13 banner image

--- calix-game.js
+++ calix-game.js
@@ -1284,6 +1284,7 @@
       4: 'Images/04_Banner/EP04_Banner.jpeg',
       6: 'Images/04_Banner/EP06_Banner.png',
       7: 'Images/04_Banner/EP07_Banner.jpeg',
+      13: 'Images/04_Banner/EP13_Banner.jpeg',
       17: 'Images/04_Banner/EP17_Banner.jpeg',
       24: 'Images/04_Banner/EP24_Banner.jpeg',
       27: 'Images/04_Banner/EP27_Banner.png',
